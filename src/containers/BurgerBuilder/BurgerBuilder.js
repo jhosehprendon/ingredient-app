ecbad2ab0bd822cf6101e547c9700c4ed3a31e1f
Jhosehp <jhosehp.rendon@gmail.com>
2018-10-26 10:46:07 -0500

    Seting up Redux and reducers

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,17 +26,17 @@ class BurgerBuilder extends Component {
         error: false
     }
 
-    componentDidMount () {
-        axios.get('https://react-burger-20b9b.firebaseio.com/ingreadients.json')
-            .then(response => {
-                this.setState({
-                    ingredients: response.data
-                })
-            })
-            .catch(error => {
-                this.setState({ error: true })
-            })
-    }
+    // componentDidMount () {
+    //     axios.get('https://react-burger-20b9b.firebaseio.com/ingreadients.json')
+    //         .then(response => {
+    //             this.setState({
+    //                 ingredients: response.data
+    //             })
+    //         })
+    //         .catch(error => {
+    //             this.setState({ error: true })
+    //         })
+    // }
 
     updatePurchaseState (ingredients) {
         const sum = Object.keys(ingredients)
