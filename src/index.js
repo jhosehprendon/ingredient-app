b1f18dc50ff542519d3c7473292df9d08fa2fdf0
Jhosehp <jhosehp.rendon@gmail.com>
2018-10-31 10:49:33 -0500

    Checking Auth timeout and login out automatically

--- src/index.js
+++ src/index.js
@@ -7,15 +7,18 @@ import * as serviceWorker from './serviceWorker';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
-    order: orderReducer
+    order: orderReducer,
+    auth: authReducer
 })
 
 const store = createStore(rootReducer, composeEnhancers(
